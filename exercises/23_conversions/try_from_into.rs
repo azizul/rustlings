c746bdc1ae52a863c97763b35d178dc9575ab0ff
Azizul Rahman Azizan <azizul.rahman.azizan@gmail.com>
2024-02-07 18:13:07 +0800

    some koan fixed

--- exercises/23_conversions/try_from_into.rs
+++ exercises/23_conversions/try_from_into.rs
@@ -27,8 +27,6 @@ enum IntoColorError {
     IntConversion,
 }
 
-// I AM NOT DONE
-
 // Your task is to complete this implementation and return an Ok result of inner
 // type Color. You need to create an implementation for a tuple of three
 // integers, an array of three integers, and a slice of integers.
@@ -41,6 +39,18 @@ enum IntoColorError {
 impl TryFrom<(i16, i16, i16)> for Color {
     type Error = IntoColorError;
     fn try_from(tuple: (i16, i16, i16)) -> Result<Self, Self::Error> {
+        let color = match tuple {
+            (r, g, b) if r < 0 || r > 255 || g < 0 || g > 255 || b < 0 || b > 255 => {
+                return Err(IntoColorError::IntConversion)
+            }
+            (r, g, b) => Color {
+                red: r as u8,
+                green: g as u8,
+                blue: b as u8,
+            },
+        };
+
+        Ok(color)
     }
 }
 
@@ -48,6 +58,18 @@ impl TryFrom<(i16, i16, i16)> for Color {
 impl TryFrom<[i16; 3]> for Color {
     type Error = IntoColorError;
     fn try_from(arr: [i16; 3]) -> Result<Self, Self::Error> {
+        let color = match arr {
+            [r, g, b] if r < 0 || r > 255 || g < 0 || g > 255 || b < 0 || b > 255 => {
+                return Err(IntoColorError::IntConversion)
+            }
+            [r, g, b] => Color {
+                red: r as u8,
+                green: g as u8,
+                blue: b as u8,
+            },
+        };
+
+        Ok(color)
     }
 }
 
@@ -55,6 +77,26 @@ impl TryFrom<[i16; 3]> for Color {
 impl TryFrom<&[i16]> for Color {
     type Error = IntoColorError;
     fn try_from(slice: &[i16]) -> Result<Self, Self::Error> {
+        if slice.len() != 3 {
+            return Err(IntoColorError::BadLen);
+        }
+        let color = match slice {
+            &[r, g, b] if r < 0 || r > 255 || g < 0 || g > 255 || b < 0 || b > 255 => {
+                return Err(IntoColorError::IntConversion)
+            }
+            &[r, g, b] => Color {
+                red: r as u8,
+                green: g as u8,
+                blue: b as u8,
+            },
+            &[] | &[_, _] | &[_, _, _, _, ..] | &[_] => Color {
+                red: 0u8,
+                green: 0u8,
+                blue: 0u8,
+            },
+        };
+
+        Ok(color)
     }
 }
 
